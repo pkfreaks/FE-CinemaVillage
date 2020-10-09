7902472e9ad58271ee871e7a3faef3507772a2e1
Bartosz Pawełczyk <bartosz.pawelczyk@softnauts.com>
2020-10-09 11:32:03 +0200

    Initial commit

---
+++ src/app/routes/routes.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RoutesRoutingModule } from './routes-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RoutesRoutingModule],
+})
+export class RoutesModule {}
